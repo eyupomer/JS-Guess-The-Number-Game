8eaff17f2c3edbe13060667d3d680ce7729e7930
eyupomer <odabasieyupomer@gmail.com>
2022-04-19 22:21:06 +0300

    working with message part

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+/*
+console.log( document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!'
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10
+document.querySelector('.guess').value = 23;
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No number!';
+
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈 Too high!'
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = '📉 Too low!'
+    }
+
+
+});
