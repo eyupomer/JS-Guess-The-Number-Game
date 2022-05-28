a045950fc5ff1277fce4bd628b963a46e5e133f1
coder554011 <m.furkan1140@gmail.com>
2022-05-28 15:47:50 +0300

    keypress function added

--- script.js
+++ script.js
@@ -58,3 +58,37 @@ document.querySelector('.again').addEventListener('click', function () {
     document.querySelector('.number').style.width = '15rem';
     displayMessage('Start guessing...');
 })
+
+document.addEventListener('keydown', function(e) {
+    if ( e.key === 'Enter') {
+        const guess = Number(document.querySelector('.guess').value);
+
+    // When there is no input
+    if (!guess) {
+        displayMessage('⛔ No number!');
+
+        // When player wins
+    } else if (guess === secretNumber) {
+        displayMessage('🎉 Correct Number!');
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+        // When guess is wrong
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('❌ You lost the game!');
+            document.querySelector('.score').textContent = 0;
+        }
+    } 
+    };
+});
